d5e009ae36da625e5dd619334154214808899730
culozik <eugenbah@gmail.com>
2025-02-12 19:14:00 +0200

    feat: add logic for sending data to the bot

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import useTelegram from "../../hooks/useTelegram";
 
@@ -10,6 +10,25 @@ const Form = () => {
 	const [street, setStreet] = useState("");
 	const [subject, setSubject] = useState("physical");
 	const { tg } = useTelegram();
+
+	const onSendData = useCallback(() => {
+		const data = {
+			country,
+			city,
+			street,
+			subject,
+		};
+
+		tg.sendData(JSON.stringify(data));
+	}, []);
+
+	useEffect(() => {
+		tg.onEvent("mainButtonClick", onSendData);
+
+		return () => {
+			tg.offEvent("mainButtonClick", onSendData);
+		};
+	}, []);
 
 	useEffect(() => {
 		tg.MainButton.setParams({
